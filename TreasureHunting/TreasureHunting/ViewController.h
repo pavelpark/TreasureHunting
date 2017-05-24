1761c616e2397120a5adaf728e6e689261b50fbb
Pavel Parkhomey <pavelpark@aol.com>
2017-05-24 08:24:28 -0700

    added gitignore

---
+++ TreasureHunting/TreasureHunting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TreasureHunting
+//
+//  Created by Pavel Parkhomey on 5/24/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TreasureHunting/TreasureHunting/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TreasureHunting
+//
+//  Created by Pavel Parkhomey on 5/24/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
